686a914957e334fe41a9875b1855cbcf0ba6d059
Robson Barbosa <robson.barbosa12@gmail.com>
2023-06-19 12:55:45 -0300

    created new projetc to class room errorGitHubt

---
+++ TestToGithubError.java
@@ -0,0 +1,23 @@
+package JustTest;
+
+public class TestToGithubError {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	System.out.println("Qualquer coisa");
+	System.out.println("Qualquer coisa 2");
+	System.out.println("Qualquer coisa 3");
+	
+	for (int i = 0; i < args.length; i++) {
+		for (String string : args) {
+			
+		}
+	}
+	
+	
+	
+	}
+
+}
---
+++ TestToGithubError2.java
@@ -0,0 +1,23 @@
+package JustTest;
+
+import java.util.Scanner;
+
+public class TestToGithubError2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("Qualquer coisa");
+		System.out.println("Qualquer coisa 2");
+		System.out.println("Qualquer coisa 3");
+		
+		for (int i = 0; i < args.length; i++) {
+			for (String string : args) {
+				
+			}
+		}
+	sc.close();
+	}
+
+}
